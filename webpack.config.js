0e7a757b80bb19f5a448791895d8919856eb63b1
Ryan Williams <ryan@runsascoded.com>
2022-01-30 10:51:38 -0500

    working plugin checkpoint

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-console.log(HTMLWebpackPlugin)
+console.log(HTMLWebpackPlugin, require.resolve('html-webpack-plugin'))
 // import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+
 var HscryptPlugin = require('hscrypt-webpack-plugin').default;
-console.log(HscryptPlugin)
+console.log(HscryptPlugin, require.resolve('hscrypt-webpack-plugin'))
 
 module.exports = {
     entry: './src/index.ts',
@@ -16,16 +20,28 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         MiniCssExtractPlugin.loader,
+            //         "css-loader"
+            //     ]
+            // }
         ],
     },
     resolve: {
-        extensions: [ '.ts', '.js', ],
+        extensions: [ '.ts', '.js', /*'.css',*/ ],
     },
     plugins: [
         new HTMLWebpackPlugin({
             inject: false,
             template: 'index.html',
         }),
+        // new MiniCssExtractPlugin({
+        //     filename: "[name].css",
+        //     chunkFilename: "[id].css"
+        // }),
+        // new HTMLInlineCSSWebpackPlugin({ HTMLWebpackPlugin }),
         new HscryptPlugin({
             // HTMLWebpackPlugin,
             filename: 'demo.bundle.js',
