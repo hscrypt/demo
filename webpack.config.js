fdbc25e2147aa9142a8b802968439a2a55e9894e
Ryan Williams <ryan@runsascoded.com>
2022-01-30 11:14:39 -0500

    use released hscrypt-webpack-plugin, clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-console.log(HTMLWebpackPlugin, require.resolve('html-webpack-plugin'))
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-// const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
-
-var HscryptPlugin = require('hscrypt-webpack-plugin').default;
-console.log(HscryptPlugin, require.resolve('hscrypt-webpack-plugin'))
+const HscryptPlugin = require('hscrypt-webpack-plugin').default;
 
 module.exports = {
     entry: './src/index.ts',
@@ -20,30 +12,17 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         MiniCssExtractPlugin.loader,
-            //         "css-loader"
-            //     ]
-            // }
         ],
     },
     resolve: {
-        extensions: [ '.ts', '.js', /*'.css',*/ ],
+        extensions: [ '.ts', '.js', ],
     },
     plugins: [
         new HTMLWebpackPlugin({
             inject: false,
             template: 'index.html',
         }),
-        // new MiniCssExtractPlugin({
-        //     filename: "[name].css",
-        //     chunkFilename: "[id].css"
-        // }),
-        // new HTMLInlineCSSWebpackPlugin({ HTMLWebpackPlugin }),
         new HscryptPlugin({
-            // HTMLWebpackPlugin,
             filename: 'demo.bundle.js',
             path: 'dist',
             hscrypt: '../../js/dist/hscrypt.bundle.js',
@@ -53,11 +32,5 @@ module.exports = {
     output: {
         filename: 'demo.bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        // libraryTarget: 'var',
-        // library: 'foo',
-        // clean: true,
     },
-    // optimization: {
-    //     runtimeChunk: 'single',
-    // },
 };
